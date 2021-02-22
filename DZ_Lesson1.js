76bc7fd61dfa829cd9df658d3fa7cda46906b5a4
Stepan <stepanbor@gmail.com>
2021-02-22 20:03:19 +0300

    firsta dz for lesson 1

---
+++ DZ_Lesson1.js
@@ -0,0 +1,20 @@
+'use strict';
+var tempC;
+tempC=100;
+console.log(tempC);
+var tempF;
+tempF=(9 / 5) * tempC + 32;
+console.log(tempF);
+alert(tempF+' F');
+
+
+var admin;
+var naMe='Василий';
+console.log(typeof admin);
+console.log(typeof naMe);
+console.log(naMe);
+admin=naMe;
+console.log(typeof admin);
+console.log(admin);
+
+console.log(1000 + "108"); // 
